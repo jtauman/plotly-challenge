54b553b8f81dfe5edba0c5d502337fda6a195abf
James Auman <jtauman@nc.rr.com>
2020-09-14 14:53:04 -0400

    first draft of initial bar plot

--- belly_button_diversity/static/js/app.js
+++ belly_button_diversity/static/js/app.js
@@ -2,15 +2,48 @@
 d3.json("../../data/samples.json").then((importedData) => {
     console.log(importedData);
     var data = importedData;
-    console.log(data.names);
+    console.log("Names:" , data.names);
+    console.log("Metadata:", data.metadata);
+    console.log("Samples:", data.samples);
+    
+    var sampleID = data.samples[0].id;
+    var sampleResults = data.samples[0].sample_values;
+    var otuIDs = data.samples[0].otu_ids;
+    var otuLabels = data.samples[0].otu_labels;
+
+    console.log("Sample ID:", sampleID);
+    console.log("Sample Results:", sampleResults);
+    console.log("OTU IDs:", otuIDs);
+    console.log("OTU Lables:", otuLabels);
+
+    var trace1 = {
+        x: sampleResults,
+        y: otuIDs,
+        type: 'bar',
+        orientation: 'h'
+    }
+
+    var data = [trace1];
+
+    Plotly.newPlot("bar", data);
+});
+
+
+
+
+    //initialize graphs
+    // function init() {
+    //     var id = data.samples.
+    // }
+
     //add options to button
-    d3.select("#selDataset")
-        .selectAll("option")
-        .data(data)
-        .enter()
-        .append("option")
-        .attr("value", function(option) {return option.names; })
-        .text(function(option) {return option.names;})
+    // d3.select("#selDataset")
+    //     .selectAll("option")
+    //     .data(data)
+    //     .enter()
+    //     .append("option")
+    //     .attr("value", function(option) {return option.names; })
+    //     .text(function(option) {return option.names;})
 
     // Submit Button handler
     // function handleSubmit() {
@@ -24,4 +57,3 @@ d3.json("../../data/samples.json").then((importedData) => {
     // };
 
 
-});
