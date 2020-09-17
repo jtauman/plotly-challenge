6e8d176c53a7706735f42e4a1531840676a705bb
James Auman <jtauman@nc.rr.com>
2020-09-16 21:02:57 -0400

    fixed bar plot, added updating on change

--- belly_button_diversity/static/js/app.js
+++ belly_button_diversity/static/js/app.js
@@ -11,9 +11,37 @@ function init() {
         //populate dataset options to button
         var dropdown = document.getElementById("selDataset");
         for (index in data.names) {
-            console.log(data.names[index])
+            //console.log(data.names[index])
             dropdown.options[dropdown.options.length] = new Option(data.names[index], index[index]);
         };
+
+        //bar graph
+        var topResults = data.samples[0].sample_values.slice(0,10);
+        var topOTUs = data.samples[0].otu_ids.slice(0,10);
+        var stringOtuIDs = topOTUs.map(i => "OTU " + i);
+        var topOTULabels = data.samples[0].otu_labels.slice(0,10);
+        console.log(topResults);
+        console.log(topOTUs);
+        console.log(stringOtuIDs);
+        console.log(topOTULabels);
+
+        //horizontal bargraph trace
+        var traceBar = {
+            x: topResults.reverse(),
+            y: stringOtuIDs.reverse(),
+            text: topOTULabels.reverse(),
+            type: 'bar',
+            orientation: 'h'
+        }
+
+        var layoutBar = {
+            title: "Top 10 Belly Button Bacteria Counts",
+            width: 450
+        }
+
+        var dataBar = [traceBar];
+
+        Plotly.newPlot("bar", dataBar, layoutBar);
 
         //populate demographic data
         var demoData = data.metadata[0];    
@@ -31,10 +59,41 @@ init()
 function optionChanged (value){
     d3.json("../../data/samples.json").then((importedData) => {
         console.log(importedData)
+        //filter metadata
         var targetData = importedData.metadata.filter(e => {
             return e.id === parseInt(value)
-        })
+        });
         console.log(targetData)
+        //filter sample data
+        var targetSampleData = importedData.samples.filter(e => {
+            return e.id === (value)
+        });
+        console.log(targetSampleData);
+
+        //update bar graph
+        var newSampleData = Object.values(targetSampleData);
+        var newTopResults = newSampleData[0].sample_values.slice(0,10);
+        var newTopOTUs = newSampleData[0].otu_ids.slice(0,10);
+        var newStringOtuIDs = newTopOTUs.map(i => "OTU " + i);
+        var newTopOTULabels = newSampleData[0].otu_labels.slice(0,10);
+
+        //horizontal bargraph trace
+        var traceBar = {
+            x: newTopResults.reverse(),
+            y: newStringOtuIDs.reverse(),
+            text: newTopOTULabels.reverse(),
+            type: 'bar',
+            orientation: 'h'
+        }
+
+        var layoutBar = {
+            title: "Top 10 Belly Button Bacteria Counts",
+            width: 450
+        }
+
+        var dataBar = [traceBar];
+
+        Plotly.newPlot("bar", dataBar, layoutBar);
 
         //populate demographic data
         var newMetaData = Object.values(targetData);    
@@ -44,5 +103,5 @@ function optionChanged (value){
             console.log(`${key}: ${value}`)
             d3.select("#sample-metadata").append("h6").text(`${key}: ${value}`)
         });
-    })
+    });
 };
